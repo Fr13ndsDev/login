3d40a4787d8126c5f82713a2dc89cc528c833f48
Javoxir Yoshuzaqov <javohiroken707@gmail.com>
2024-03-06 15:52:46 +0500

    update code

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google"; 
+const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
+export const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
+
+// export default NextAuth(authOptions);
